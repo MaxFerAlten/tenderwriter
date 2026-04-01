72af11817161eecae9c28706de1086b2ff90fab3
MaxFerAlten <maxferalten@gmail.com>
2026-04-01 19:59:45 +0200

    fix keycloak mustermott regretion

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 const apiTarget = process.env.VITE_API_TARGET || 'http://localhost:8000';
+const mattermostTarget = process.env.VITE_MM_TARGET || 'http://localhost:8065';
 
 export default defineConfig({
   plugins: [react()],
@@ -17,6 +18,12 @@ export default defineConfig({
         target: apiTarget,
         changeOrigin: true,
         ws: true,
+      },
+      '/mm': {
+        target: mattermostTarget,
+        changeOrigin: true,
+        ws: true,
+        xfwd: true,
       },
     },
   },
